3e725c0465b4442a810713f8e065628b0f099db7
Daikeen Lockett <daikeen_lockett@yahoo.com>
2022-11-15 18:25:13 -0500

    Appended the scoreText and the timerCount to the DOM

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,15 @@
 var scoreEl = document.querySelector("#score");
 // Selects Element with 'timer' ID
 var timerEl = document.querySelector("#timer");
+
+
+var scoreText = document.createElement("h2");
+var timerCount = document.createElement("h2")
+
+
+scoreText.textContent = "High Scores";
+timerCount.textContent = "90"
+
+// Appends Elements to the DOM
+scoreEl.appendChild(scoreText)
+timerEl.appendChild(timerCount)
